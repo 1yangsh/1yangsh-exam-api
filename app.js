dd5e42276e2f5cd71b33af9d44eb10cb9ff16a1a
1yangsh <ysh410@gmail.com>
2021-02-03 17:03:54 +0900

    modified app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.get('/', (req, res) => res.send('This system is running.'))
 // http://127.0.0.1:3000/service?status=on
 app.get('/service', (req, res) => {
     var status = req.param('status')
-    res.send('This system is running [Status : ' + status + ']');
+    res.send('The system is running [Status : ' + status + ']');
 });
 
 
