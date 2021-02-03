29d8e5f251d666ab987b9b911178274c88962375
1yangsh <ysh410@gmail.com>
2021-02-03 16:57:02 +0900

    uploaded an example api

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const express = require('express')
 const app = express();
 const port = process.env.PORT || 3000;
 // http://127.0.0.1:3000/
-app.get('/', (req, res) => res.send('Hello, Nodejs! and Heroku!'))
-// http://127.0.0.1:3000/welcome?name=seunghyeon
-app.get('/welcome', (req, res) => {
-    var user_name = req.param('name')
-    res.send('Hi, there. Welcome to the Nodejs service. [' + user_name + ']');
+app.get('/', (req, res) => res.send('This system is running.'))
+// http://127.0.0.1:3000/service?status=on
+app.get('/service', (req, res) => {
+    var status = req.param('status')
+    res.send('This system is running [Status : ' + status + ']');
 });
 
 
